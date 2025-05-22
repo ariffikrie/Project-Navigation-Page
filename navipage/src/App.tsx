b23a8b1551107143f99347fa2eef46fa207cd07d
ARIF FIKRIE <ariffikrie99@gmail.com>
2025-05-22 16:04:30 +0800

    adding sparkles component from aceternity

--- navipage/src/App.tsx
+++ navipage/src/App.tsx
@@ -1,14 +1,11 @@
 // import Projects from './components/Projects';
 import "@radix-ui/themes/styles.css";
-import { Theme } from "@radix-ui/themes";
 
 function App() {
   return (
     <html>
       <body>
-        <Theme accentColor="indigo">
-          {/* Project */}
-        </Theme>
+
       </body>
     </html>
   );
